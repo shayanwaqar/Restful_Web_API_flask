d1269c2004bff31b30e65a164a4960bd7ad1ebe2
Shayan Waqar <110746653+shayanwaqar@users.noreply.github.com>
2025-08-07 19:46:01 -0400

    Added routes for articles and individual articles via ids

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def landing_page():
+def api_root():
     return 'Welcome to the landing page'
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<article_id>')
+def api_article(article_id):
+    return 'You are reading article: ' + article_id
+
+if __name__ == 'main':
+    app.run()
