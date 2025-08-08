b06a5a151a1c3c065139040997eb1f36049f7a31
Shayan Waqar <110746653+shayanwaqar@users.noreply.github.com>
2025-08-08 15:27:30 -0400

    Added hello route that takes in an arg and outputs based on it.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Python flask RESTful Web API
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +13,13 @@ def api_articles():
 @app.route('/articles/<article_id>')
 def api_article(article_id):
     return 'You are reading article: ' + article_id
+
+@app.route('/hello')
+def hello():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello John Doe'
 
 if __name__ == '__main__':
     app.run()
