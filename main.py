b9f00bba27d27c997e2b20825ee2b56ad5c73942
Shayan Waqar <110746653+shayanwaqar@users.noreply.github.com>
2025-08-11 19:15:19 -0400

    error handling added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Python flask RESTful Web API
 import json
-from flask import Flask, url_for, request, json
+from flask import Flask, url_for, request, json, Response
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,25 +24,31 @@ def hello():
 
 @app.route('/echo', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def api_echo():
-    if request.method == 'GET':
-        return 'echo: GET\n'
-    elif request.method == 'POST':
-        return 'echo: POST\n'
-    else:
-        return 'echo: other HTTP method'
+    try:
+        if request.method == 'GET':
+            return 'echo: GET\n'
+        elif request.method == 'POST':
+            return 'echo: POST\n'
+        else:
+            return 'echo: other HTTP method'
+    except:
+        print('ERROR')
     
 @app.post("/messages")
 def messages():
-    ctype = (request.content_type or "").lower()
-    if ctype.startswith("text/plain"): #clients and curl often sends 'text/plain; charset=utf-8'
-        return "Text Message: " + request.get_data(as_text=True)
-    if request.is_json:
-        return "JSON Message: " + json.dumps(request.get_json())
-    if ctype.startswith("application/octet-stream"):
-        with open("binary", "wb") as f:
-            f.write(request.get_data())
-        return "Binary message written!"
-    return "415 Unsupported Media Type ;)", 415
+    try: 
+        ctype = (request.content_type or "").lower()
+        if ctype.startswith("text/plain"): #clients and curl often sends 'text/plain; charset=utf-8'
+            return "Text Message: " + request.get_data(as_text=True)
+        if request.is_json:
+            return "JSON Message: " + json.dumps(request.get_json())
+        if ctype.startswith("application/octet-stream"):
+            with open("binary", "wb") as f:
+                f.write(request.get_data())
+            return "Binary message written!"
+        return "415 Unsupported Media Type ;)", 415
+    except:
+        print('ERROR')
 
 if __name__ == "__main__":
     app.run(debug=True) # this will prevent the need to restart server after every change.
